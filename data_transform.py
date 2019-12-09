64a3f17f9ff5b6e6834187b0f8514b6967206128
Arnaud Bressot <arnaud.bressot@lilo.org>
2019-12-09 21:39:32 +0100

    Remove derniere modif car non pertinente

--- data_transform.py
+++ data_transform.py
@@ -3,23 +3,17 @@ from datetime import datetime
 
 def toDataCategorie(data_dict):
     data_cat = {}
-    vid_cat = {}
     for video in data_dict:
         for t in data_dict[video]["evolution"]:
             d = datetime.utcfromtimestamp(t["time"]).strftime('%d-%m-%Y %H:%M:%S')
             if not(d in data_cat):
                 data_cat[d] = {}
             for c in data_dict[video]["categories"]:
-                if not(c in vid_cat):
-                    vid_cat[c] = 0
                 if not(c in data_cat[d]):
                     data_cat[d][c] = 0
-                vid_cat[c] = vid_cat[c] + 1
                 data_cat[d][c] = data_cat[d][c] + t["views"]
     for t in data_cat:
-        for c in data_cat[t]:
-            data_cat[t][c] = data_cat[t][c] / vid_cat[c]
-        d = sorted(data_cat[t].items(), key=lambda t: t[1])
+        d = sorted(data_cat[t].items(), reverse=True, key=lambda t: t[1])
         data_cat[t] = d
     return data_cat
 
