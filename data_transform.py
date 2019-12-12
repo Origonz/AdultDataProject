265a549e98f732873e2d6a2c2fa3bd17195f35dd
Arnaud Bressot <arnaud.bressot@lilo.org>
2019-12-12 18:59:28 +0100

    Fix video deleted

--- data_transform.py
+++ data_transform.py
@@ -2,6 +2,28 @@ import json
 from datetime import datetime, timedelta
 import sys
 
+# Netoyeur de données
+# si une vidéo est supprimé, on garde son nombre de vue avant suppression
+def DataClean(data_dict):
+    for video in data_dict:
+        tmp = 0
+        for t in data_dict[video]["evolution"]:
+            if t["percent"] == -1:
+                t["views"] = tmp
+            else:
+                tmp = t["views"]
+    return data_dict
+
+# Transforme les tuples en dictionnaire
+def tupleToDict(data_tuple):
+    data_dict = {}
+    for d in data_tuple:
+        data_dict[d] = {}
+        for c in data_tuple[d]:
+            data_dict[d][c[0]] = c[1]
+    return data_dict
+
+# Transforme les données en views par categorie
 def toDataCategorie(data_dict):
     data_cat = {}
     for video in data_dict:
@@ -16,9 +38,10 @@ def toDataCategorie(data_dict):
     for t in data_cat:
         d = sorted(data_cat[t].items(), reverse=True, key=lambda t: t[1])
         data_cat[t] = d
-    return data_cat
+    return tupleToDict(data_cat)
 
-def toDayView(data_dict): #A refaire
+# compte le nombre de vue pour chaque jour de la semaine
+def toDayView(data_dict):
     data_day = {}
     for video in data_dict:
         tmp_date = {}
@@ -39,16 +62,9 @@ def toDayView(data_dict): #A refaire
                 data_day[d] = 0
             data_day[d] = data_day[d] + v
     return data_day
-    
-def tupleToDict(data_tuple):
-    data_dict = {}
-    for d in data_tuple:
-        data_dict[d] = {}
-        for c in data_tuple[d]:
-            data_dict[d][c[0]] = c[1]
-    return data_dict
 
-def toTimeView(data_dict): #A refaire - nb de vue qui diminue
+# Compte le nombre de vue en fonction de la date
+def toTimeView(data_dict):
     data_time = {}
     for video in data_dict:
         for t in data_dict[video]["evolution"]:
@@ -58,10 +74,13 @@ def toTimeView(data_dict): #A refaire - nb de vue qui diminue
             data_time[d] = data_time[d] + t["views"]
     return data_time
 
+
 with open('data.json') as json_data:
     data_dict = json.load(json_data)
 
-    data_day = toDayView(data_dict)
+    data_dict = DataClean(data_dict)
+
+    data_day = toTimeView(data_dict)
 
     for d in data_day:
         print(d + " : " + str(data_day[d]))
