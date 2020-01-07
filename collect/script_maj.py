49e0e0a89f3d97335d32c2c575ba828ceba70750
VCanete <valentin.canete@gmail.com>
2020-01-07 15:51:36 +0100

    update script

--- collect/script_maj.py
+++ collect/script_maj.py
@@ -41,6 +41,18 @@ for x in data:
                     time = None
                 data[x]['duration'] = time 
 
+            max_view = 0
+            for e in data[x]['evolution']:
+                if e['views'] > max_view:
+                    max_view = e['views']
+            try:
+                duration = int(data[x]['duration'])
+                data[x]['cost'] = (duration * max_view) * 0.25
+            except ValueError:
+                continue
+            except TypeError:
+                continue
+
 
 with open("data.json", "w") as f:
     json.dump(data, f)
