fb558c0149c6841afc99ff2137c139c9dc6f65f0
VCanete <valentin.canete@gmail.com>
2020-01-08 17:36:58 +0100

    add script et vue

--- collect/script_maj_pub.py
+++ collect/script_maj_pub.py
@@ -5,36 +5,40 @@ from datetime import datetime
 import time
 import re
 
-with open("data.json", "r") as f:
+with open("../data/data.json", "r") as f:
     data = json.load(f)
 
 
 time = datetime.timestamp(datetime.now())
+count = 0
 
 for x in data:
-    if "duration" not in data[x] or data[x]['duration'] is None:
+    if "duration" not in data[x] or data[x]['duration'] == "None":
+        print("-------")
+        print("Champ vide : ", data[x]['duration'])
         url = "https://fr.pornhub.com" + x
         with requests.get(url, stream=True) as r:
+            print("Connexion")
             html = r.text
             soup = BeautifulSoup(html, features="lxml")
-            det = soup.find_all(class_="video-action-tab about-tab active")
-            if len(det) == 0 :
-                view = -1
-                percent = -1
-                data[x]['tags'] = 'deleted'
-                data[x]['duration'] = 'None'
-            else:
-                try:
-                    time_script = soup.find_all(class_="original mainPlayerDiv")[0].find("script")
-                    pattern = re.compile("\"video_duration\":\"\d+\"") 
-                    var = pattern.search(time_script.text)
-                    time = var.group().split("\"")[3]
-                except AttributeError:
-                    time = None
-                data[x]['duration'] = time 
+            print(soup.find_all(class_="original mainPlayerDiv"))
+            time_script = soup.find_all(class_="original mainPlayerDiv")[0].find("script")
+            print("Parcours scrip")
+            pattern = re.compile("\"video_duration\":\"\d+\"") 
+            var = pattern.search(time_script.text)
+            time = var.group().split("\"")[3]
+            print("Valeur récupéré : ", time)
+
+            # except AttributeError:
+            #     print("Pub ", count)
+            #     time = "None"
+            #     count += 1
+            data[x]['duration'] = time 
+
+print("Nombre de vidéos sans durée : ", count)
 
 
-with open("data.json", "w") as f:
+with open("../data/data.json", "w") as f:
     json.dump(data, f)
 
 
